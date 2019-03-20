3e9a83b6dce8adf6d780da7e315e25a0c58c05c5
Walther de Groot <walther@ttt.nl>
2019-03-20 10:44:53 +0100

    Initial commit

---
+++ Indie_Game/Assets/Scripts/CameraScripts/FollowingCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingCamera : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position + offset;
+    }
+}
