0d95aa5879f27241ddedeb48fc62e219b8ee1ff8
Walther de Groot <walther@ttt.nl>
2019-03-26 10:59:12 +0100

    #7 First Person View and Control Added

---
+++ Indie_Game/Assets/Scripts/CameraScripts/CameraController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Public variables
+    public float minX = -60f;
+    public float maxX = 60f;
+    public float minY = -360f;
+    public float maxY = 360f;
+
+    public float sensitivityX = 15f;
+    public float sensitivityY = 15f;
+
+    public Camera cam;
+
+    // Private Variables
+    float rotationY = 0f;
+    float rotationX = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        rotationY += Input.GetAxis("Mouse X") * sensitivityY;
+        rotationX += Input.GetAxis("Mouse Y") * sensitivityX;
+
+        rotationX = Mathf.Clamp(rotationX, minX, maxX);
+
+        transform.localEulerAngles = new Vector3(0, rotationY, 0);
+        cam.transform.localEulerAngles = new Vector3(-rotationX, rotationY, 0);
+
+        Vector3 desiredPosition = transform.position;
+        cam.transform.position = desiredPosition;
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+}
---
+++ Indie_Game/Assets/Scripts/UserScripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //public variables
+    public float movementSpeed = 5f;
+
+    //private variables
+    Rigidbody rb;
+    Vector3 moveDirection;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        var horizontalMovement = Input.GetAxis("Horizontal");
+        var verticalMovement = Input.GetAxis("Vertical");
+
+        moveDirection = (horizontalMovement * transform.right + verticalMovement * transform.forward).normalized;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            print("Shoot");
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            print("Zoom");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 yFix = new Vector3(0, rb.velocity.y, 0);
+
+        rb.velocity = moveDirection * movementSpeed * (Time.deltaTime / 2);
+        rb.velocity += yFix;
+    }
+
+    void Shoot()
+    {
+        
+    }
+}
